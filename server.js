403ce484e813fabdca7d045b0350297ff0ac62da
bharatgabrani-sbdinc <bharat.gabrani@sbdinc.com>
2022-11-03 13:35:06 +0530

    First Commit

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const path = require('path');
+const compression = require('compression');
+
+const app = express();
+const port = 7200;
+
+app.use(bodyParser.json()); // convert request body to Json format
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+
+  app.use(compression())
+
+  app.use(express.static(path.join(__dirname, 'client/build')));
+
+  app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+
+
+
+app.listen(port, error => {
+  if (error) throw error;
+  console.log('Server running on port ' + port);
+});
+
