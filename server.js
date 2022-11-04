74714c7a7a72c63894198eed5dfaae48afd8ab27
bharatgabrani-sbdinc <bharat.gabrani@sbdinc.com>
2022-11-04 22:01:55 +0530

    Update of port

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require("path");
 const compression = require("compression");
 
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 app.use(bodyParser.json()); // convert request body to Json format
 app.use(bodyParser.urlencoded({ extended: true }));
