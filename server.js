139bc4eb9df97a57e0bf136581e95a48d8fa0506
bharatgabrani-sbdinc <bharat.gabrani@sbdinc.com>
2022-11-04 20:35:00 +0530

    Added file to giignore

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const path = require('path');
-const compression = require('compression');
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const path = require("path");
+const compression = require("compression");
 
 const app = express();
 const port = 8080;
@@ -11,19 +11,18 @@ app.use(bodyParser.json()); // convert request body to Json format
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
+app.use(compression());
 
-  app.use(compression())
+app.use(express.static(path.join(__dirname, "client/build")));
 
-  app.use(express.static('./client/build/'));
-
-  app.get('/*', function (req, res) {
-    res.sendFile('index.html', { root: __dirname + '/client/build/' });
-  });
-
-
-
-app.listen(port, error => {
-  if (error) throw error;
-  console.log('Server running on port ' + port);
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
+app.get("/test", function (req, res) {
+  res.send({ message: "hello" });
 });
 
+app.listen(port, (error) => {
+  if (error) throw error;
+  console.log("Server running on port " + port);
+});
